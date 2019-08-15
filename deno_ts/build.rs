8259cc7d0ffabbbf53f4a5098d1663865a679180
Ryan Dahl <ry@tinyclouds.org>
2019-08-15 01:49:43 -0400

    Reorg - remove unnecessary parts

--- deno_ts/build.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-  println!("cargo:rerun-if-changed=deno_ts/src/main.js");
-  println!("cargo:rerun-if-changed=deno_ts/src/typescript.js");
-}
--- example/src/bundle.ts
+++
@@ -1,3 +0,0 @@
-function add(x: number, y: number): number {
-  return x + y;
-}
