87e30a1ed62b3da57baff624bbb86818117c6a6b
Ryan Dahl <ry@tinyclouds.org>
2019-08-20 13:54:34 -0400

    Remove unused file

--- cli_snapshots/main.ts
+++
@@ -1,9 +0,0 @@
-const window = (0, eval)("this");
-
-import { printHello } from "./print_hello.ts";
-
-function add(x: number, y: number): number {
-  return x + y;
-}
-window.add = add;
-window.printHello = printHello;
